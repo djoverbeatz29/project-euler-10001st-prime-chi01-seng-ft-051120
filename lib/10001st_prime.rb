384c57e042b916ee2f9c20d4ef5fe9d64af1c2cb
jequezinho <jroverby92@gmail.com>
2020-09-09 15:05:09 -0500

    Done.

--- lib/10001st_prime.rb
+++ lib/10001st_prime.rb
@@ -1,1 +1,17 @@
-# Implement your procedural solution here!
+def prime_number_for(n)
+    i = 1
+    count = 0
+    while count < n
+        i += 1
+        is_prime = true
+        for b in (2..(i ** 0.5).floor())
+            if i % b == 0
+                is_prime = false
+            end
+        end
+        if is_prime
+            count += 1
+        end
+    end
+    i
+end
--- lib/oo_10001st_prime.rb
+++ lib/oo_10001st_prime.rb
@@ -1,1 +1,24 @@
-# Implement your object-oriented solution here!
+class Prime
+    def initialize(n)
+        @n = n
+    end
+
+    def number
+        i = 1
+        count = 0
+        while count < @n
+            i += 1
+            is_prime = true
+            for b in (2..(i ** 0.5).floor())
+                if i % b == 0
+                    is_prime = false
+                end
+            end
+            if is_prime
+                count += 1
+            end
+        end
+        i
+    end
+
+end
